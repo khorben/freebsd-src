906e3770115cf5eefd0d20a1ed1736be5419e5e6
Pierre Pronchery <khorben@FreeBSD.org>
2026-05-21 13:18:33 +0200

    Fixup

--- share/mk/bsd.sbom.mk
+++ share/mk/bsd.sbom.mk
@@ -16,46 +16,52 @@ SBOMDIR?=	${SRCTOP}/share/sbom/pkgconfig
 SPDXDIR?=	/usr/share/sbom/spdx
 SPDXTOOL?=	spdxtool
 
-.if defined(LIB)
-PCFILE?=	lib${LIB}.pc
-SBOM_TAG_ARGS=	${LIB_TAG_ARGS}
-.endif
+. if defined(LIB)
+SBOMNAME?=	lib${LIB}
+SBOMTAGS=	package=${PACKAGE:Uutilities},lib
+. elif defined(PROG)
+SBOMNAME?=	${PROG}
+SBOMTAGS=	package=${PACKAGE:Uutilities}
+. else
+SBOMNAME=
+SBOMTAGS=
+. endif # defined(PROG) || defined(LIB)
 
-.if defined(PROG)
-PCFILE?=	${PROG}.pc
-SBOM_TAG_ARGS=	${TAG_ARGS}
-.endif
+. if !empty(SBOMNAME)
+SBOMFILE?=	${SBOMNAME}.pc
+. endif # !empty(SBOMNAME)
+SBOM_TAG_ARGS=	-T ${SBOMTAGS:ts,:[*]}
 
-.if !empty(PCFILE) && exists(${SBOMDIR}/${PCFILE})
+. if !empty(SBOMFILE) && exists(${SBOMDIR}/${SBOMFILE})
 
-${PCFILE:R}.jsonld: ${SBOMDIR}/${PCFILE}
-	${SPDXTOOL} ${SBOMDIR}/${PCFILE} > ${.TARGET}
+.  if !defined(NO_JSONLD_SBOM)
+all: ${SBOMNAME}.jsonld
 
-jsonldinstall: .PHONY ${PCFILE:R}.jsonld
-	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${PCFILE:R}.jsonld \
-		${DESTDIR}${JSONLDDIR}/${PCFILE:R}.jsonld
+${SBOMNAME}.jsonld: ${SBOMDIR}/${SBOMFILE}
+	${SPDXTOOL} ${SBOMDIR}/${SBOMFILE} > ${.TARGET}
 
-${PCFILE:R}.spdx: ${SBOMDIR}/${PCFILE}
-	${BOMTOOL} ${SBOMDIR}/${PCFILE} > ${.TARGET}
-
-spdxinstall: .PHONY ${PCFILE:R}.spdx
-	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${PCFILE:R}.spdx \
-		${DESTDIR}${SPDXDIR}/${PCFILE:R}.spdx
-
-.if !defined(NO_JSONLD_SBOM)
-all: ${PCFILE:R}.jsonld
+jsonldinstall: .PHONY ${SBOMNAME}.jsonld
+	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${SBOMNAME}.jsonld \
+		${DESTDIR}${JSONLDDIR}/${SBOMNAME}.jsonld
 
 realinstall: jsonldinstall
 .ORDER: beforeinstall jsonldinstall
-.endif
+.  endif # !defined(NO_JSONLD_SBOM)
 
-.if !defined(NO_SPDX_SBOM)
-all: ${PCFILE:R}.spdx
+.  if !defined(NO_SPDX_SBOM)
+all: ${SBOMNAME}.spdx
+
+${SBOMNAME}.spdx: ${SBOMDIR}/${SBOMFILE}
+	${BOMTOOL} ${SBOMDIR}/${SBOMFILE} > ${.TARGET}
+
+spdxinstall: .PHONY ${SBOMNAME}.spdx
+	${INSTALL} ${SBOM_TAG_ARGS} -m 0644 ${SBOMNAME}.spdx \
+		${DESTDIR}${SPDXDIR}/${SBOMFILE:R}.spdx
 
 realinstall: spdxinstall
 .ORDER: beforeinstall spdxinstall
-.endif
+.  endif # !defined(NO_SPDX_SBOM)
 
-.endif	# exists(${SBOMDIR}/${PCFILE})
+. endif	# exists(${SBOMDIR}/${SBOMFILE})
 
 .endif	# ${MK_SBOM}
